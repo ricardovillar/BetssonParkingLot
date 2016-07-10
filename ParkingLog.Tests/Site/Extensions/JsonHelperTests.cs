8ef55c0a895aa01583a2474213ce29f4d28ffa42
Ricardo Villar de Oliveira <ricardovillardeoliveira@gmail.com>
2016-07-10 15:10:16 -0300

    Project creation

---
+++ ParkingLog.Tests/Site/Extensions/JsonHelperTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using ParkingLot.Site.Extensions;
+
+namespace ParkingLot.Tests.Site.Extensions {
+    public class JsonHelperTests {
+        [TestCaseSource("ObjectsAndJsons")]
+        public void ToJson_should_parse_to_json(TestObject obj, string expected) {
+            Assert.AreEqual(expected, obj.ToJson());
+        }
+
+        private static readonly object[] ObjectsAndJsons = {
+            new object[] {new TestObject { Parameter1 = 1, Parameter2 = "Value1"}, "{\"Parameter1\":1,\"Parameter2\":\"Value1\"}"},
+            new object[] {new TestObject { Parameter1 = 2, Parameter2 = "Value2"}, "{\"Parameter1\":2,\"Parameter2\":\"Value2\"}"},
+            new object[] {new TestObject { Parameter1 = 3, Parameter2 = "Value3"}, "{\"Parameter1\":3,\"Parameter2\":\"Value3\"}"},
+        };
+    }
+
+    public class TestObject {
+        public int Parameter1 { get; set; }
+        public string Parameter2 { get; set; }
+
+        public override bool Equals(object obj) {
+            if (!(obj is TestObject)) {
+                return false;
+            }
+            var that = (TestObject) obj;
+            return Parameter1 == that.Parameter1 && Parameter2 == that.Parameter2;
+        }
+    }
+}
---
+++ ParkingLot.Site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ParkingLot.Site.MvcApplication" Language="C#" %>
