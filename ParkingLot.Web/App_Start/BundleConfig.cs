030b9e3e87664e5d36b0e4ec88bb2b814c953f80
Ricardo Villar de Oliveira <ricardovillardeoliveira@gmail.com>
2016-07-20 02:27:47 -0300

    removing unnecessary libraries

--- ParkingLot.Web/App_Start/BundleConfig.cs
+++ ParkingLot.Web/App_Start/BundleConfig.cs
@@ -8,9 +8,6 @@ namespace ParkingLot.Web {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
             bundles.Add(new ScriptBundle("~/bundles/angular")
                 .Include("~/Scripts/angular.min.js")
                 .Include("~/Scripts/angular-animate.min.js"));
@@ -25,11 +22,6 @@ namespace ParkingLot.Web {
 
             bundles.Add(new ScriptBundle("~/bundles/chart").Include(
             "~/Scripts/chart.bundle.min.js"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
             
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
